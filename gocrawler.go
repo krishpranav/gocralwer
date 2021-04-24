a03a02a445b3c047b9b4fc56966b14e72bb1e61d
krishpranav <krisna.pranav@gmail.com>
2021-04-24 13:46:25 +0530

    updated options type structure

--- gocrawler.go
+++ gocrawler.go
@@ -1,26 +1,37 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/json"
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
 	"regexp"
-	"runtime"
-	"strconv"
-	"strings"
-	"sync"
-	"time"
-
-	"github.com/PuerkitoBio/goquery"
-	"github.com/jroimartin/gocui"
 )
 
 // meta data
 var VERSION = "0.1.2"
-var STATUS_LINE_NAME  = fmt.Sprintf("[gocrawler/v%s]", VERSION)
+var STATUS_LINE_NAME = fmt.Sprintf("[gocrawler/v%s]", VERSION)
 
+// option structure
+type Options struct {
+	Robots            bool
+	Sitemap           bool
+	WayBack           bool
+	IgnoreInvalidSSL  bool
+	Thread            int
+	Timeout           int
+	Delay             int
+	Depth             int
+	MaxRegexResult    int
+	URL               string
+	Proxy             string
+	RegexString       string
+	Regex             *regexp.Regexp
+	URLExclude        string
+	URLExcludeRegex   *regexp.Regexp
+	StatusCodeExclude string
+	StatusCodeRegex   *regexp.Regexp
+	InScopeExclude    string
+	Scheme            string
+	Headers           string
+	Query             string
+	Keys              []string
+	InScopeDomains    []string
+}
