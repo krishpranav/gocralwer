c16fb68784ff8826078bff055b84c5a133359bee
krishpranav <krisna.pranav@gmail.com>
2021-04-24 13:54:30 +0530

    added viewAttrs, search editor, response editor

--- gocrawler.go
+++ gocrawler.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sync"
+	"time"
 
 	"github.com/jroimartin/gocui"
 )
 
-// meta data
+// Metadata
 var VERSION = "0.1.2"
-var STATUS_LINE_NAME = fmt.Sprintf("[gocrawler/v%s]", VERSION)
+var STATUS_LINE_NAME = fmt.Sprintf("[evine/v%s]", VERSION)
 
-// option structure
+// Options structure
 type Options struct {
 	Robots            bool
 	Sitemap           bool
@@ -38,7 +40,7 @@ type Options struct {
 	InScopeDomains    []string
 }
 
-// output result structure
+// Output result structure
 type Results struct {
 	Pages        string
 	PageByURL    map[string]string
@@ -57,9 +59,68 @@ type Results struct {
 	RegMaches    map[string][]string
 }
 
-// program definitions
+// Program definitions
 type def struct {
 	currentPage      string
 	currentPageIndex int
 	Gui              *gocui.Gui
 }
+
+// CUI View Attributes
+type viewAttrs struct {
+	editor   gocui.Editor
+	editable bool
+	frame    bool
+	text     string
+	title    string
+	wrap     bool
+	x0       func(int) int
+	y0       func(int) int
+	x1       func(int) int
+	y1       func(int) int
+}
+
+// Search prompt editor struct type
+type searchEditor struct {
+	editor gocui.Editor
+}
+
+// URL editor struct type
+type singleLineEditor struct {
+	editor gocui.Editor
+}
+
+// RESPONSE editor struct type
+type responseEditor struct {
+	editor gocui.Editor
+}
+
+var (
+	// Initial OPTIONS
+	OPTIONS = &Options{}
+	// To identify media postfixes
+	MEDIA_POSTFIX = []string{"aa", "aac", "aif", "aiff", "amr", "amv", "amz", "ape", "asc", "asf", "au", "bash", "bat", "bmp", "c",
+		"cfa", "chm", "cpp", "cs", "csv", "doc", "docx", "dmg", "f4a", "f4b", "f4p", "f4v", "flac", "flv", "gif", "gif", "gifv",
+		"go", "gz", "ico", "java", "jfif", "jpeg", "jpg", "m2v", "m4a", "m4p", "m4v", "md", "mkv", "mng", "mov",
+		"mp2", "mp3", "mp4", "mpeg", "mpg", "mpv", "msi", "pdf", "pl", "png", "ppt", "pptx", "py", "rar", "rm", "roq",
+		"svg", "svi", "tar.gz", "tiff", "vmo", "vob", "w64", "wav", "webm", "wma", "wmv", "woff2", "wrk",
+		"wvavi", "xlsx", "xz", "yaml", "yml", "zip", "7z", "tgz", "exe", "psd"}
+	// Scheme://hostname.tld
+	BASEURL = ""
+	// Project Name: Hostname.tld
+	PROJECT_NAME = ""
+	VIEWS        = []string{"URL", "OPTIONS", "HEADERS", "QUERY", "REGEX", "RESPONSE", "SEARCH_PROMPT"}
+	ALL_VIEWS    = []string{"URL", "OPTIONS", "HEADERS", "QUERY", "REGEX", "RESPONSE", "SEARCH", "STATUS_LINE", "SEARCH_PROMPT"}
+	// Pre-define keys
+	ALL_KEYS    = []string{"email", "url", "query_urls", "all_urls", "phone", "media", "css", "script", "cdn", "comment", "dns", "network", "all"}
+	MIN_X       = 60
+	MIN_Y       = 20
+	VIEWS_OBJ   map[string]*gocui.View
+	VIEWS_ATTRS = map[string]viewAttrs{}
+	PROG        def
+	DEPTH       = 1
+	TOKENS      chan struct{}
+	RESULTS     *Results
+	START_TIME  time.Time
+	MUTEX       = &sync.Mutex{}
+)
