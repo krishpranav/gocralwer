a2300f50e438f3d296bc98b87efa5b91c8673aaf
krishpranav <krisna.pranav@gmail.com>
2021-04-25 10:19:41 +0530

    updated single line editor

--- gocrawler.go
+++ gocrawler.go
@@ -1264,3 +1264,40 @@ func (e searchEditor) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modi
 		return nil
 	})
 }
+
+// The singleLineEditor removes multi lines capabilities.
+// the Edit function credited from the Wuzz project
+func (e singleLineEditor) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	switch {
+	case (ch != 0 || key == gocui.KeySpace) && mod == 0:
+		e.editor.Edit(v, key, ch, mod)
+		// At the end of the line the default gocui editor adds a whitespace
+		// Force him to remove
+		ox, _ := v.Cursor()
+		if ox > 1 && ox >= len(v.Buffer())-2 {
+			v.EditDelete(false)
+		}
+		return
+	case key == gocui.KeyEnter:
+		return
+	case key == gocui.KeyArrowRight:
+		ox, _ := v.Cursor()
+		if ox >= len(v.Buffer())-1 {
+			return
+		}
+	case key == gocui.KeyHome || key == gocui.KeyArrowUp:
+		v.SetCursor(0, 0)
+		v.SetOrigin(0, 0)
+		return
+	case key == gocui.KeyEnd || key == gocui.KeyArrowDown:
+		width, _ := v.Size()
+		lineWidth := len(v.Buffer()) - 1
+		if lineWidth > width {
+			v.SetOrigin(lineWidth-width, 0)
+			lineWidth = width - 1
+		}
+		v.SetCursor(lineWidth, 0)
+		return
+	}
+	e.editor.Edit(v, key, ch, mod)
+}
